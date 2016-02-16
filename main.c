e05840948dda36ccd6963ca8024fe606417c6822
jrlink <jrlink@ncsu.edu>
2016-02-16 12:34:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
+// Joseph Link ECE461 Project1
+// Program Flash Memory
+
 #include "msp432.h"
 #include "driverlib.h"
 #include "string.h"
 
-#define BUFFER_MEMORY_START0 0x0000
+
 #define BUFFER_MEMORY_START1 0x02000
 int BUFFER_MEMORY_START_COUNT = 0x0000;
 
@@ -55,34 +58,28 @@ void main(void)
 	uint8_t buffer[4096];
 	memset(buffer,0xA5,4096);
 
-	if(sectorcount < 16){
+
+																	//clear timer constantly to prevent timeout.
+
 		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
-		MAP_FlashCtl_unprotectSector(FLASH_INFO_MEMORY_SPACE_BANK0,FLASH_SECTOR0<<sectorcount); //Unprotect current sector.
+		MAP_FlashCtl_unprotectSector(FLASH_INFO_MEMORY_SPACE_BANK1,FLASH_SECTOR0|FLASH_SECTOR1|FLASH_SECTOR2|FLASH_SECTOR3|FLASH_SECTOR4|FLASH_SECTOR5
+				|FLASH_SECTOR6|FLASH_SECTOR7|FLASH_SECTOR8|FLASH_SECTOR9|FLASH_SECTOR10|FLASH_SECTOR11|FLASH_SECTOR12|FLASH_SECTOR13|FLASH_SECTOR14
+				|FLASH_SECTOR15|FLASH_SECTOR16|FLASH_SECTOR17|FLASH_SECTOR18|FLASH_SECTOR19|FLASH_SECTOR20|FLASH_SECTOR21|FLASH_SECTOR22|FLASH_SECTOR23
+				|FLASH_SECTOR24|FLASH_SECTOR25|FLASH_SECTOR26|FLASH_SECTOR27|FLASH_SECTOR28|FLASH_SECTOR29|FLASH_SECTOR30|FLASH_SECTOR31); //Unprotect current sector.
 		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
-		BUFFER_MEMORY_START_COUNT = BUFFER_MEMORY_START0+ (4096*sectorcount);					//Memory address for the next sector to be erased and programmed.
+		BUFFER_MEMORY_START_COUNT = BUFFER_MEMORY_START1+ (4096*sectorcount);					//Memory address for the next sector to be erased and programmed.
 
 		MAP_FlashCtl_eraseSector(BUFFER_MEMORY_START_COUNT);									//Erase current sector.
 		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
 
 		MAP_FlashCtl_programMemory(buffer,(void*)BUFFER_MEMORY_START_COUNT, 4096 ); 			//Program sectors.
 		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
-		MAP_FlashCtl_protectSector(FLASH_INFO_MEMORY_SPACE_BANK0,FLASH_SECTOR0<<sectorcount);	//Protect sectors.
-		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
-	}
-	else {
-		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
-		MAP_FlashCtl_unprotectSector(FLASH_INFO_MEMORY_SPACE_BANK1,FLASH_SECTOR0<<sectorcount); //Unprotect current sector.
-		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
-		BUFFER_MEMORY_START_COUNT = BUFFER_MEMORY_START1+ (4096*(sectorcount - 16));					//Memory address for the next sector to be erased and programmed.
-
-		MAP_FlashCtl_eraseSector(BUFFER_MEMORY_START_COUNT);									//Erase current sector.
+		MAP_FlashCtl_protectSector(FLASH_INFO_MEMORY_SPACE_BANK1,FLASH_SECTOR0|FLASH_SECTOR1|FLASH_SECTOR2|FLASH_SECTOR3|FLASH_SECTOR4|FLASH_SECTOR5
+				|FLASH_SECTOR6|FLASH_SECTOR7|FLASH_SECTOR8|FLASH_SECTOR9|FLASH_SECTOR10|FLASH_SECTOR11|FLASH_SECTOR12|FLASH_SECTOR13|FLASH_SECTOR14
+				|FLASH_SECTOR15|FLASH_SECTOR16|FLASH_SECTOR17|FLASH_SECTOR18|FLASH_SECTOR19|FLASH_SECTOR20|FLASH_SECTOR21|FLASH_SECTOR22|FLASH_SECTOR23
+				|FLASH_SECTOR24|FLASH_SECTOR25|FLASH_SECTOR26|FLASH_SECTOR27|FLASH_SECTOR28|FLASH_SECTOR29|FLASH_SECTOR30|FLASH_SECTOR31);	//Protect sectors.
 		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
 
-		MAP_FlashCtl_programMemory(buffer,(void*)BUFFER_MEMORY_START_COUNT, 4096 ); 			//Program sectors.
-		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
-		MAP_FlashCtl_protectSector(FLASH_INFO_MEMORY_SPACE_BANK1,FLASH_SECTOR0<<sectorcount);	//Protect sectors.
-		WDTCTL = WDTPW | WDTCNTCL;																//clear timer constantly to prevent timeout.
-	}
     sectorcount++;																			//increase counter for number of sectors already wrote to.
 
     if (sectorcount>=32){																	//This if statement is for when we have wrote to all 32 sectors of 4KB,
@@ -90,7 +87,7 @@ void main(void)
     	delay_1sec();																		//is reset via the watchdog counter
     	P2OUT &= ~BIT1;
     	sectorcount = 0;
-    	WDTCTL = (WDTPW-1);
+
     }
 	}
 }
